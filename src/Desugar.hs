3a0324152deb73f3b32e869e0187a48beccda571
Peter Burtsev <52280455+Litoprobka@users.noreply.github.com>
2026-05-13 16:58:13 +0300

    rewrite pattern matching desugar (no proper lifting for now)

--- src/Desugar.hs
+++ src/Desugar.hs
@@ -31,8 +31,8 @@ case_ :: AdjConstructors -> CoreTerm -> [(EPattern, CoreTerm)] -> CoreTerm
 case_ _ arg [] = C.Case arg C.CaseWD{branches = C.ConCase Map.empty, def = Nothing}
 case_ adjConstructors arg (m : rest) =
     let ?adjConstructors = adjConstructors
-     in let caseTree = foldl1' (merge const (const True)) $ fmap (uncurry toTree) (m :| rest)
-         in fromTree (\_ term -> term) arg (Level 0) caseTree
+     in let caseTree = foldl1' merge $ fmap (\(pat, body) -> toTree [pat] body) (m :| rest)
+         in fromTree [const arg] (Level 0) caseTree
 
 match :: AdjConstructors -> [(NonEmpty (Typed EPattern), CoreTerm)] -> CoreTerm
 match _ [] = error "empty match"
@@ -40,9 +40,10 @@ match adjConstructors (m@(pats, _) : rest) =
     let ?adjConstructors = adjConstructors
      in let len = length pats
             types = toList $ fmap (\(_ ::: ty) -> ty) pats
-            mkBranch (pats, body) = toTreeNested (toList $ fmap E.unTyped pats) body
-            tree = foldl1' (mergeNested const (const True)) $ fmap mkBranch (m :| rest)
-            body = fromNested (\lvl term -> C.liftAt lvl.getLevel (Level len) term) (Level len) (fmap Level [0 .. pred len]) tree
+            mkBranch (pats, body) = toTree (toList $ fmap E.unTyped pats) body
+            tree = foldl1' merge $ fmap mkBranch (m :| rest)
+            args = (\argLvl newLvl -> C.Var $ levelToIndex newLvl (Level argLvl)) <$> [0 .. pred len]
+            body = fromTree args (Level len) tree
          in foldr (\(i, ty) -> C.Lambda Visible (Name' $ "x" <> show @_ @Int i) ty) body (zip [0 ..] types)
 
 list :: CoreTerm -> [CoreTerm] -> CoreTerm
@@ -63,40 +64,37 @@ variantType row = C.TyCon VariantName $ (Visible, C.Row row) :< Nil
 recordType :: ExtRow CoreType -> CoreTerm
 recordType row = C.TyCon RecordName $ (Visible, C.Row row) :< Nil
 
-data Nested a
-    = NotNested a
-    | Nested (CaseTree (Nested a))
-    deriving (Show)
-
 data CaseTree a
-    = Leaf a -- do we need something akin to as-patterns? if we merge a var-branch with a con-branch,
-    -- we'd have to shift indices in both
-    -- although we'd have to shift indices either way
-    -- when merging a var-branch with different con-branches (e.g. Just and Nothing)
-    --
-    -- 'Leaf' seems kinda redundant, given that it represents the same thing as an empty branch with a default case
-    -- what about `newtype CaseTree = CaseTree (These (CaseTreeNE a) a)`?
-    | Branch (CaseTreeNE a) (Maybe a)
+    = Leaf a
+    | Var (Maybe SimpleName_) (CaseTree a) -- var patterns are separate from CaseBranch, because they can't have a default case nor any branching
+    | Branch (CaseBranch a) (Maybe (CaseTree a))
     deriving (Show)
 
 -- here, visibilities and names are only preserved for pretty-printing
 data Args = Args Int [(Visibility, SimpleName_)] deriving (Show)
 
-newtype CaseTreeNE a
-    = ConB (IdMap Name_ (Args, Nested a))
-    -- we don't really need 'Nested', because branches for a specific constructor always have the same amount of arguments
-    -- and when merging a Branch with a wildcard, we also know how many subbranches to skip
+newtype CaseBranch a
+    = ConB (IdMap Name_ (Args, CaseTree a))
     deriving (Show)
 
 -- todo: all branch bodies should be extracted to outer scoped let-bound lambdas
 -- that way, we'd be able to avoid renaming shenanigans
 -- extracting their types may prove to be... complicated
 
-toTree :: EPattern -> a -> CaseTree a
-toTree pat body = case pat of
-    E.VarP _ -> Leaf body
-    E.WildcardP _ -> Leaf body
-    E.ConstructorP con args -> Branch (ConB $ IdMap.one con (mkArgs args, toTreeNested (snd <$> toList args) body)) Nothing
+type AdjConstructors =
+    IdMap
+        Name_ -- any constructor of a type
+        (IdMap Name_ (Vector (Visibility, CoreType)))
+
+toTree :: [EPattern] -> a -> CaseTree a
+toTree [] body = Leaf body
+toTree (pat : pats) body = case pat of
+    E.VarP name -> Var (Just name) (toTree pats body)
+    E.WildcardP name -> Var (Just $ Wildcard' name) (toTree pats body)
+    E.ConstructorP con args ->
+        let conPats = snd <$> toList args
+            subtree = toTree (conPats <> pats) body
+         in Branch (ConB $ IdMap.one con (mkArgs args, subtree)) Nothing
     _ -> error "desugar other patterns: todo"
   where
     mkArgs args = Args (length args) (zipWith (\i (vis, argPat) -> (vis, argName i argPat)) [0 ..] args)
@@ -105,18 +103,13 @@ toTree pat body = case pat of
         E.WildcardP txt -> Wildcard' txt
         _ -> Name' $ "y" <> show i
 
-toTreeNested :: [EPattern] -> a -> Nested a
-toTreeNested [] x = NotNested x
-toTreeNested (pat : pats) x = Nested $ toTree pat (toTreeNested pats x)
-
-type AdjConstructors =
-    IdMap
-        Name_ -- any constructor of a type
-        (IdMap Name_ (Vector (Visibility, CoreType)))
-
-fromTree :: (?adjConstructors :: AdjConstructors) => (Level -> a -> CoreTerm) -> CoreTerm -> Level -> CaseTree a -> CoreTerm
-fromTree toTerm arg lvl = \case
-    Leaf body -> toTerm lvl body
+fromTree :: (?adjConstructors :: AdjConstructors) => [Level -> CoreTerm] -> Level -> CaseTree CoreTerm -> CoreTerm
+fromTree [] _lvl (Leaf body) = body
+fromTree [] _lvl _ = error "fromTree: tree requires more arguments than given"
+fromTree (mkArg : args) lvl tree = case tree of
+    Leaf body -> body -- short-circuit case. Do we need to do something with the index?
+    Var (Just name) subtree -> C.Let name arg (fromTree args (succ lvl) subtree)
+    Var Nothing subtree -> fromTree args lvl subtree
     Branch (ConB cases) Nothing -> C.Case arg $ C.CaseWD (mkBranches cases) Nothing
     -- we drop the catch-all case when all patterns are covered,
     -- and turn the catch-all case into a normal case when all but one are covered
@@ -139,64 +132,65 @@ fromTree toTerm arg lvl = \case
                     | IdMap.size cases == IdMap.size constrs ->
                         C.Case arg $ C.CaseWD (mkBranches cases) Nothing
                     | IdMap.size cases == IdMap.size constrs - 1 ->
-                        -- maybe we should construct a nested tree of the proper length here, I'm not sure
-                        let lastCase = (\vises -> (mkArgs vises, NotNested def)) <$> IdMap.difference constrs cases
+                        let lastCase = (\vises -> (mkArgs vises, wrapInVars (Vec.length vises) def)) <$> IdMap.difference constrs cases
                          in C.Case arg $ C.CaseWD (mkBranches $ cases <> lastCase) Nothing
-                _ -> C.Case arg $ C.CaseWD (mkBranches cases) (Just (Wildcard' "_", toTerm lvl def))
+                _ -> C.Case arg $ C.CaseWD (mkBranches cases) (Just (Wildcard' "_", fromTree args (succ lvl) def))
   where
+    arg = mkArg lvl
+
     mkBranches cases =
         C.ConCase $
-            cases & Map.mapWithKey \name (Args n args, nested) -> (C.ConstructorP name args, fromNested toTerm (lvl `incLevel` n) [lvl .. lvl `incLevel` pred n] nested)
+            cases & Map.mapWithKey \name (Args n argNames, subtree) ->
+                let conArgs = (\argLvl newLvl -> C.Var $ levelToIndex newLvl argLvl) <$> [lvl .. lvl `incLevel` pred n]
+                 in (C.ConstructorP name argNames, fromTree (conArgs <> args) (lvl `incLevel` n) subtree)
+
     mkArgs vises = Args (Vec.length vises) $ ((,Wildcard' "_") . fst) <$> toList vises
 
--- this should pass the indexes
-{-
-  case _ of
-    (x, y, z) -> case x@2 of
-      w -> case x@3 of
-        _ -> _
--}
-fromNested :: (?adjConstructors :: AdjConstructors) => (Level -> a -> CoreTerm) -> Level -> [Level] -> Nested a -> CoreTerm
-fromNested toTerm lvl = \cases
-    _ (NotNested term) -> toTerm lvl term
-    (argLvl : lvls) (Nested nested) -> fromTree (flip (fromNested toTerm) lvls) (C.Var $ levelToIndex lvl argLvl) lvl nested
-    _ _ -> error "nested-pattern length mismatch"
+wrapInVars :: Int -> CaseTree a -> CaseTree a
+wrapInVars 0 subtree = subtree
+wrapInVars n subtree = Var Nothing (wrapInVars (pred n) subtree)
 
-{- | merge two CaseTrees
-@merge mergeSubtree isSubtreeCovering@
--}
-merge :: (a -> a -> a) -> (a -> Bool) -> CaseTree a -> CaseTree a -> CaseTree a
-merge f isCovering = \cases
-    (Leaf lhs) (Leaf rhs) -> Leaf $ f lhs rhs
-    (Branch (ConB lhs) lhsDef) (Leaf x) -> Branch (ConB $ (fmap . fmap) (flip mergeNested' (NotNested x)) lhs) (Just $ mergeDefaultR f lhsDef x)
-    (Leaf x) (Branch (ConB rhs) rhsDef)
-        | isCovering x -> Leaf x
-        | otherwise -> Branch (ConB $ (fmap . fmap) (mergeNested' (NotNested x)) rhs) (Just $ mergeDefaultL f x rhsDef)
-    -- is this right? should we also merge the default case into the branches?
-    -- probably not, because the per-constructor branches are always at least as specific as the fallback branch
-    (Branch lhs lhsDef) (Branch rhs rhsDef) -> Branch (mergeNE f isCovering (lhs, lhsDef) (rhs, rhsDef)) mergedDefaults
+merge :: CaseTree a -> CaseTree a -> CaseTree a
+merge = \cases
+    leaf@Leaf{} _ -> leaf
+    lhs _ | isCovering lhs -> lhs
+    (Var lhsName lhs) (Var rhsName rhs) -> Var (lhsName <|> rhsName) (merge lhs rhs)
+    (Var name lhs) leaf@Leaf{} -> Var name (merge lhs leaf) -- may cause a level mismatch
+    (Branch (ConB lhs) lhsDef) leaf@Leaf{} ->
+        let cases = lhs & (fmap . fmap) (`merge` leaf)
+         in Branch (ConB cases) (Just $ mergeDefaultR merge lhsDef leaf)
+    -- these two cases may cause a level mismatch, I'm not sure
+    (Branch (ConB lhs) lhsDef) (Var _ rhs) ->
+        let cases = lhs & (fmap . fmap) (`merge` rhs)
+         in Branch (ConB cases) (Just $ mergeDefaultR merge lhsDef rhs)
+    (Var _ lhs) (Branch (ConB rhs) rhsDef) ->
+        let cases = rhs & (fmap . fmap) (merge lhs)
+         in Branch (ConB cases) (Just $ mergeDefaultL merge lhs rhsDef)
+    (Branch lhs lhsDef) (Branch rhs rhsDef) -> Branch (mergeBranch (lhs, lhsDef) (rhs, rhsDef)) mergedDefaults
       where
         mergedDefaults = case (lhsDef, rhsDef) of
-            (Just lhsDef, Just rhsDef) -> Just $ f lhsDef rhsDef
+            (Just lhsDef, Just rhsDef)
+                | isCovering lhsDef -> Just lhsDef
+                | otherwise -> Just $ merge lhsDef rhsDef
             (lhsDef, Nothing) -> lhsDef
             (Nothing, rhsDef) -> rhsDef
   where
-    mergeNested' = mergeNested f isCovering
+    isCovering = \case
+        Leaf{} -> True
+        Var _ subtree -> isCovering subtree
+        Branch _ (Just def) -> isCovering def
+        _ -> False
 
-{- | merge two CaseTreeNEs with optional default cases
-@mergeNE mergeSubtree isSubtreeCovering@
--}
-mergeNE :: (a -> a -> a) -> (a -> Bool) -> (CaseTreeNE a, Maybe a) -> (CaseTreeNE a, Maybe a) -> CaseTreeNE a
-mergeNE f isCovering (ConB lhs, lhsDef) (ConB rhs, rhsDef) =
+mergeBranch :: (CaseBranch a, Maybe (CaseTree a)) -> (CaseBranch a, Maybe (CaseTree a)) -> CaseBranch a
+mergeBranch (ConB lhs, lhsDef) (ConB rhs, rhsDef) =
     ConB $
-        IdMap.merge
-            (leftBiasedArg mergeNested')
-            (fmap \l -> mergeDefaultL mergeNested' l (fmap NotNested rhsDef))
-            (fmap \r -> mergeDefaultR mergeNested' (fmap NotNested lhsDef) r)
+        Map.merge
+            (leftBiasedArg merge)
+            (\(args@(Args n _), l) -> (args, mergeDefaultL merge l (wrapInVars n <$> rhsDef)))
+            (\(args@(Args n _), r) -> (args, mergeDefaultR merge (wrapInVars n <$> lhsDef) r))
             lhs
             rhs
   where
-    mergeNested' = mergeNested f isCovering
     leftBiasedArg :: (a -> a -> a) -> (b, a) -> (b, a) -> (b, a)
     leftBiasedArg f (arg, l) (_, r) = (arg, f l r)
 
@@ -205,22 +199,3 @@ mergeDefaultL = foldl'
 
 mergeDefaultR :: (a -> b -> b) -> Maybe a -> b -> b
 mergeDefaultR f = flip (foldr f)
-
-mergeNested :: (a -> a -> a) -> (a -> Bool) -> Nested a -> Nested a -> Nested a
-mergeNested f isCovering = \cases
-    (NotNested lhs) _ | isCovering lhs -> NotNested lhs
-    (NotNested lhs) (NotNested rhs) -> NotNested $ f lhs rhs
-    (Nested lhs) (NotNested x) -> Nested $ innerMerge lhs (Leaf (NotNested x))
-    (NotNested x) (Nested rhs) -> Nested $ innerMerge (Leaf (NotNested x)) rhs
-    (Nested lhs) (Nested rhs) -> Nested $ innerMerge lhs rhs
-  where
-    innerMerge = merge (mergeNested f isCovering) isCoveringNested
-    isCoveringNested = \case
-        NotNested x -> isCovering x
-        Nested (Leaf x) -> isCoveringNested x
-        Nested (Branch _ (Just def)) -> isCoveringNested def
-        Nested _ -> False
-
--- if this proves to not be enough, we can check that:
--- - all cases are covered or there's a default branch
--- - all branches are covering or default branch is covering
